004cc55c4f778cb026ccbfbae0d099ec723ea363
SThampan <saritha.thampan@gmail.com>
2017-10-26 12:31:15 -0700

    Version 3

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-var state={
-	listItems : []
-};
+var state=[];
 //add items to state
-var addItem=function(state,item)
+function createItem(item) {
+  return {
+    name: item,
+    isChecked: false,
+    unCheck: function() {
+      this.isChecked=!(this.isChecked);
+    }
+    }
+  };
+var addItem=function(state,newItem)
 {
 	var found=false;
-	for (var i=0; i<state.listItems.size;i++){
-		if (item == state.listItems[i])
-			 found= true;
-	}
+	state.forEach(function (item){
+		if (item.name===newItem)
+			found=true;
+	} )
 	if (found===false)
-		state.listItems.push(item);
+		state.push(createItem(newItem));
 }
 //render functions
 var renderShoppingList=function(state,element)
 {
 	
-	var itemsHtml=state.listItems.map(function(item){
-		return '<li><span class="shopping-item">'+item+
+	
+	var itemsHtml=state.map(function(item){
+		var itemToggle ='';
+	//	console.log(item.name + ' '+ item.isChecked);
+		if (item.isChecked===true)
+		{
+			 itemToggle ='shopping-item__checked';
+		}
+			return '<li><span class="shopping-item '+itemToggle+' ">'+item.name+
         '</span> <div class="shopping-item-controls">'
-          +'<button class="shopping-item-toggle">'
+          +'<button class="shopping-item-toggle ">'
            +'<span class="button-label">check</span>'
           +'</button>'
          +'<button class="shopping-item-delete">'
            +'<span class="button-label">delete</span>'
          +'</button></div></li>';});
+	//console.log(itemsHtml);
 	
 	element.html(itemsHtml);
 	
@@ -36,16 +51,39 @@ $('#js-shopping-list-form').submit(function(event)
 	event.preventDefault();
 	 addItem(state,$('#shopping-list-entry').val());
 	renderShoppingList(state, $('.shopping-list'));
+	$('#shopping-list-entry').val('');
 });
 $(".shopping-list").on('click','.shopping-item-toggle',function(event)
 {
+
+
+
 		var clickedElement=$(this);
-		$(clickedElement).closest("li").children(".shopping-item").toggleClass("shopping-item__checked");
+		var closestElement=$(clickedElement).closest("li").children(".shopping-item");
+		$(closestElement).toggleClass("shopping-item__checked");
+		state.forEach(function(item){
+			if (item.name===$(closestElement).text())
+					item.unCheck();
+		})
+		renderShoppingList(state, $('.shopping-list'));
 });
 $(".shopping-list").on('click','.shopping-item-delete',function(event)
 {
-	 	var clickedElement=$(this);
-	 	var itemName=$(this).closest("li").children(".shopping-item").val();
-	 	$(this).closest("li").remove();
-	 	state.listItems.pop(itemName);
+		//console.log('before delete',state);
+	
+	  	var clickedElement=$(this);
+	 	var itemName=$(this).closest("li").children(".shopping-item").text();
+	 	//console.log(itemName);
+	 	var i=state.find(function(item){
+	 		//console.log(item.name+' '+itemName);
+	 			return (item.name===itemName);
+
+		})
+		//console.log(i);
+		var deleteIndex=state.indexOf(i);
+		//console.log(deleteIndex);
+		 state.splice(deleteIndex,1);
+		//console.log('after delete',state);
+	renderShoppingList(state, $('.shopping-list')); 
+
 });
